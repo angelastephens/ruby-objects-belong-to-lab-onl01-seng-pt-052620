7b3e09b7091c000a110030881e25cf875935389f
Angela Stephens <angelastephens317@gmail.com>
2020-06-20 05:14:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,9 @@
 class Song
-  attr_accessor :name, :title
+  attr_accessor :name,:title, :artist
   
-  def initialize(name, title)
+  def initialize(name, title, artist)
     @name=name
     @title=title
+    @artist=artist
   end
 end
